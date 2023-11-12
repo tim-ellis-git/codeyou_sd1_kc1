3d4a1f85f697d3d6a823aa7454e8c10da1fdbbda
Tim <lanigironueht@gmail.com>
2023-11-12 11:36:35 -0500

    partial optional work, ran out of time

---
+++ KnowledgeCheck1_Calculator/GetInputs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 
+// Plan to make a validator method to verify the inputs and recursively call the method until a good input received 
+// if 'dog' is entered it would return "not a valid number try again" until an acceptable one entered for the 
+// do for both inputs (maybe allow for more), then use these returns as the input parameters of main method 
+// only calling the get input to validate and appropriate math method
+
+namespace KnowledgeCheck1_Calculator
+{
+    internal class GetInputs
+    {
+        public double validateInputOne() { 
+            var inputOne = Console.ReadLine();
+            if (double.TryParse(inputOne, out double numberOne)) {
+                return numberOne;
+            }
+            else
+            {
+                Console.WriteLine("One or more of the numbers is not a valid number, try again");
+                Console.ReadLine(); 
+            }
+        }  
+    }
+}
+
+*/
